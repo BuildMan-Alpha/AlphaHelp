282c2c3df9fda12f0e087844cd56137e52e0844f
cian-chambliss <cian@alphasoftware.com>
2016-02-07 17:50:40 -0500

    Example markup

---
+++ buildhelp/example/test.js
@@ -0,0 +1,29 @@
+
+var formobject = {
+/*[DOC:
+Context:formobject
+Constructor: FormObject(foo,bar)
+Arguments:
+ foo (string|number)- fooish argument
+ bar (string:optional) - barish argument
+Description: make a form object
+]*/
+formObject: function(foo,bar) {    
+} ,
+/*[DOC:
+Method: preview(reportname,id)
+Description: preview a report in a div with an id. 
+Arguments:
+ reportname (string) - name of report
+ id - id of frame.
+Example:```
+if( x < 0 )
+   {dialogobject}.preview("customerList@projectReport","preview_id");
+```
+See Also:
+formobject get_pdf
+]*/
+preview : function() {
+    // some code
+}
+};
