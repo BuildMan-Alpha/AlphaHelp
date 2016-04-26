c38e955d6939a02862f9d19f5927ec05d8cde73b
Cian Chambliss <cianchambliss@Cians-MBP.netgear.com>
2016-04-25 20:53:38 -0400

    favicon

--- helpserver/helpnosearch.js
+++ helpserver/helpnosearch.js
@@ -719,6 +719,13 @@ app.use("/", function(req, res) {
         }, 0, 20);
     } else if (req.path.substring(0, 8) == '/images/') {
         res.redirect("/help" + req.path);
+    } else if( req.path.indexOf('/favicon.ico') >= 0 ) {
+        require('fs').readFile("./assets/favicon.ico",function(err,data) {
+           if( !err && data ) {
+                res.setHeader('Content-Type', 'image/png');
+                res.send(data);
+            }
+        });
     } else {
         help.expressuse(req, res);
     }
