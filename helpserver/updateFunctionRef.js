6f92ceefbfe9538bf1f92683c3a3e07cc12d4482
cian-chambliss <cian@alphasoftware.com>
2016-09-24 09:43:06 -0400

    Added logic to extract functions and methods in a list

---
+++ helpserver/updateFunctionRef.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+var zlib = require('zlib');
+fs.readFile("../generated/list.json","utf8",function(err,data) {    
+    if( !err ) {
+        var funcArray = [];
+        var filearr = JSON.parse(data);
+        var i;
+        for( i = 0 ; i < filearr.length ; ++i ) {
+            var ptr = filearr[i];
+            if( ptr.path.indexOf('/AlphaHelp/helpfiles/Ref') > 0 ) {
+                var nocasepath =  ptr.path.toLowerCase();
+                if( ptr.path.indexOf("/Desktop_Api/") > 0 || ptr.path.indexOf("/Api/") > 0 ) {
+                    if( nocasepath.indexOf(" function.xml") > 0 || nocasepath.indexOf(" method.xml") > 0 ) {
+                        var functionName = ptr.path.substring(ptr.path.lastIndexOf("/")+1);
+                        //var url = "/documentation/page/"+ptr.path.substr( ptr.path.indexOf('/AlphaHelp/helpfiles/')+21 );
+                        functionName = functionName.substring( 0 , functionName.lastIndexOf(' ') );
+                        funcArray.push(functionName);
+                    }
+                }
+            }
+        }
+        fs.writeFile("../helpfiles/functionList.json" , JSON.stringify(funcArray) );
+    }
+});
