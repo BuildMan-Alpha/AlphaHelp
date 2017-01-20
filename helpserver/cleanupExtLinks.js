e48e6e41b138e80f95f56794713211c6ebb94bd6
cian-chambliss <cian@alphasoftware.com>
2017-01-20 14:59:21 -0500

    feedback on delete

--- helpserver/cleanupExtLinks.js
+++ helpserver/cleanupExtLinks.js
@@ -4,23 +4,25 @@ var client = new elasticsearch.Client({
   log: 'trace'
 });
 client.search({
-    index: 'helpserver',
-    body: {
-      query: {  match :  { "path" : "https"} } ,
-      _source: [ "id"]
+  index: 'helpserver',
+  body: {
+    query: { match: { "path": "https" } },
+    _source: ["id"]
+  }
+}, function (error, response) {
+  if (!error) {
+    var i;
+    for (i = 0; i < response.hits.hits.length; ++i) {
+      (function () {
+        var id = response.hits.hits[i]._id;
+        if (id.indexOf(":https://github.com") > 0) {
+          client.delete({
+            index: 'helpserver',
+            type: 'helppage',
+            id: id,
+          }, function (error, response) { if (error) { console.log(error); } else { console.log("removed "+id);}  });
+        }
+      } ());
     }
-  }, function (error, response) {
-      if( !error ) {
-          var i;
-          for( i = 0 ; i < response.hits.hits.length ; ++i ) {
-              var id = response.hits.hits[i]._id;
-              if( id.indexOf(":https://github.com") > 0 )  {
-                client.delete({
-                index: 'helpserver', 
-                type: 'helppage',
-                id: id,
-                }, function (error, response) { if(error) { console.log(error); } }); 
-              }
-          }
-      }
+  }
 });
