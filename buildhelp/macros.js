56079f29c4fbf850e49cb020e4606cf3b03e2e2c
Sarah Mitchell <sarah.mitchell@alphasoftware.com>
2017-06-23 16:43:57 -0400

    fixed type in macros and updated help files

--- buildhelp/macros.js
+++ buildhelp/macros.js
@@ -1,5 +1,5 @@
 module.exports = {
-    expGetCtrllMsg: function(env, args) {
+    expGetCtrlMsg: function(env, args) {
         return ["// To get a pointer to the " + args + " class see {dialog.object}.getControl"];
     }
 };
