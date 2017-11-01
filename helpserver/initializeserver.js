47913a6244df80d45fc9761acce66aaaa860b4f0
cian-chambliss <cian@alphasoftware.com>
2017-11-01 09:18:35 -0400

    Remove webhook so that help() constructor won't open the listener

--- helpserver/initializeserver.js
+++ helpserver/initializeserver.js
@@ -1,25 +1,26 @@
 var options = require("./settings");
 var Help = require('helpserver');
+options.webhookPort = null;
 var help = Help(options);
 
 // First build the table of contents
-help.status(function (stats) {
-	if (options.search && !stats.indexServiceRunning) {
-		console.log('Cannot initialize indexes without '+options.search.provider+' instance running.');
-	} else {
-		help.generate(function (err, result) {
-			if (err)
-				console.log("Error: " + err);
-			else {
-				console.log('Help generated');
-				// Then build the index
-				help.buildindex(function (err, result) {
-					if (err)
-						console.log(err);
-					else
-						console.log('Indexes built ' + JSON.stringify(result, null, " "));
-				});
-			}
-		});
-	}
+help.status(function(stats) {
+    if (options.search && !stats.indexServiceRunning) {
+        console.log('Cannot initialize indexes without ' + options.search.provider + ' instance running.');
+    } else {
+        help.generate(function(err, result) {
+            if (err)
+                console.log("Error: " + err);
+            else {
+                console.log('Help generated');
+                // Then build the index
+                help.buildindex(function(err, result) {
+                    if (err)
+                        console.log(err);
+                    else
+                        console.log('Indexes built ' + JSON.stringify(result, null, " "));
+                });
+            }
+        });
+    }
 });
