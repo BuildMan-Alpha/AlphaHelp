249b8adfb1d8aa3c0d013cfb4a4aef1ad3984273
cian chambliss <cian@alphasoftware.com>
2024-10-02 09:58:45 -0400

    Added systemd files (need to test these changes, then document in README)

---
+++ helpserver/helpserverservice.sh
@@ -0,0 +1,2 @@
+export HOME="/root"
+/usr/bin/nodejs /home/AlphaHelp/helpserver/helpserver.js >> /var/log/node.log 2>&1
---
+++ helpserver/helptransformservice.sh
@@ -0,0 +1,2 @@
+export HOME="/root"
+/usr/bin/nodejs /home/AlphaHelp/helpserver/helpserver.js -s /home/AlphaHelp/helpserver/transform.json >> /var/log/nodetransform.log 2>&1
